c4cc28fbef6a82115c494657a018958422a3f5cc
Nathan-E <eziokwubunduchibueze.com>
2019-03-15 13:50:18 +0100

    route to get the bear resource created

--- routes/bear.js
+++ routes/bear.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 //gets the bear resource
-router.get('/', (req, res) => {
-  
+router.get('/', async (req, res) => {
+  //finds the bear collection and sort it by name
+  const bears = Bear.find().sort('name');
+  res.send(bears);
 })
 

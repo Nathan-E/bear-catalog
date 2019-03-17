6476571bd695575af7ecd2e88acd3b5adc247a76
Nathan-E <eziokwubunduchibueze.com>
2019-03-17 21:59:38 +0100

    entry point of the app created in the root directory - index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const morgan = require('morgan');
+const bears = require('./routes/bear');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
